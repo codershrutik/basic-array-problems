9376fae7bad3bcc8f4b6bcd328f065a6855e8060
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-05-22 18:10:43 +0530

    Add files via upload

---
+++ reverseArr.java
@@ -0,0 +1,24 @@
+public class reverseArr {
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 22, 55, 34, 67 };
+        reverse(arr);
+    }
+
+    static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
